1d5bd287230746dbc6bee7fe4dfab5ac9c06cee3
pocketful <iveta.jac@gmail.com>
2022-05-27 17:59:07 +0300

    signing jwt, passwordsMatch to helpers

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,19 +1,24 @@
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config');
 const { loginUserDb } = require('../models/loginModel');
+const { passwordsMatch } = require('../utils/helpers');
 
 async function loginUser(req, res) {
   const emailInput = req.body.email;
-  console.log('emailInput ===', emailInput);
   const passwordInput = req.body.password;
-  console.log('passwordInput ===', passwordInput);
   try {
     // if user with this email exists
     const foundUser = await loginUserDb(emailInput);
     console.log('user with this email ===', foundUser);
     if (!foundUser) throw new Error(400);
+
     // if passwords match
-    if (!bcrypt.compareSync(passwordInput, foundUser.password)) throw new Error(400);
-    return res.json({ success: true, message: 'Login success.' });
+    if (!passwordsMatch(passwordInput, foundUser.password)) throw new Error(402);
+
+    // signing jwt
+    const token = jwt.sign({ userId: foundUser.id }, jwtSecret, { expiresIn: '1h' });
+
+    return res.json({ success: true, message: 'Login success.', token });
   } catch (err) {
     console.log('error in login controller:', err);
     if (err.message === '400') {
