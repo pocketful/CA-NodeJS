ca2dda1251e6ef492fae016bea6a887e3fbcd959
pocketful <iveta.jac@gmail.com>
2022-05-28 14:27:30 +0300

    FE validation for user registration

---
+++ public/js/modules/validation.js
@@ -0,0 +1,148 @@
+// eslint-disable-next-line import/no-mutable-exports
+export let errorsFeArr = [];
+
+export function clearFeErrsArr() {
+  errorsFeArr = [];
+}
+
+const errSpanEls = document.querySelectorAll('.err');
+const errCommonEl = document.querySelector('.err-common');
+
+export function clearErrors() {
+  clearFeErrsArr();
+  errCommonEl.textContent = '';
+  errSpanEls.forEach((errSpanEl) => {
+    const error = errSpanEl;
+    error.textContent = '';
+    error.previousElementSibling.classList.remove('err-input');
+  });
+}
+
+export function handleErrors(errors = errorsFeArr) {
+  console.log('Errors: ', errors);
+
+  if (typeof errors === 'string') {
+    errCommonEl.textContent = errors;
+  }
+
+  if (Array.isArray(errors)) {
+    errors.forEach((errObj) => {
+      const errEl = document.getElementById(errObj.field);
+      errEl.classList.add('err-input');
+      errEl.nextElementSibling.textContent = errObj.message;
+    });
+  }
+}
+
+function addErrToErrsArr(message, field) {
+  errorsFeArr.push({ message, field });
+}
+
+function checkRequired(value, field) {
+  if (value === '') {
+    addErrToErrsArr(`${field} is not allowed to be empty`, field);
+    return true;
+  }
+  return false;
+}
+
+function checkMinLength(value, minLength, field) {
+  if (value.length < minLength) {
+    addErrToErrsArr(`${field} length must be at least ${minLength} characters long`, field);
+    return true;
+  }
+  return false;
+}
+
+function checkMaxLength(value, maxLength, field) {
+  if (value.length > maxLength) {
+    addErrToErrsArr(
+      `${field} length must be less than or equal to ${maxLength} characters long`,
+      field,
+    );
+    return true;
+  }
+  return false;
+}
+
+function checkEmail(value, field) {
+  if (!value.includes('@')) {
+    addErrToErrsArr(`${field} must be a valid email`, field);
+    return true;
+  }
+  if (!value.split('@')[1].includes('.')) {
+    addErrToErrsArr(`${field} must be a valid email`, field);
+    return true;
+  }
+  return false;
+}
+
+function checkRef(value, field, valueR, fieldR) {
+  if (value !== valueR) {
+    addErrToErrsArr("passwords does't match", fieldR);
+    addErrToErrsArr("passwords does't match", field);
+    return true;
+  }
+  return false;
+}
+
+export function checkInput(valueToCheck, field, rulesArr) {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const rule of rulesArr) {
+    if (rule === 'required') {
+      if (checkRequired(valueToCheck, field)) {
+        return;
+      }
+      // if (valueToCheck === '') {
+      //   addErrToErrsArr(`${field} is not allowed to be empty`, field);
+      //   return;
+      // }
+    }
+    if (rule.split('-')[0] === 'minLength') {
+      const min = rule.split('-')[1];
+      if (checkMinLength(valueToCheck, min, field)) {
+        return;
+      }
+      // if (valueToCheck.length < length) {
+      //   addErrToErrsArr(`${field} length must be at least ${length} characters long`, field);
+      //   return;
+      // }
+    }
+    if (rule.split('-')[0] === 'maxLength') {
+      const max = rule.split('-')[1];
+      if (checkMaxLength(valueToCheck, max, field)) {
+        return;
+      }
+      // if (valueToCheck.length > length) {
+      // eslint-disable-next-line max-len
+      //   addErrToErrsArr(`${field} length must be less than or equal to ${length} characters long`, field);
+      //   return;
+      // }
+    }
+    if (rule === 'email') {
+      if (checkEmail(valueToCheck, field)) {
+        return;
+      }
+      // if (!valueToCheck.includes('@')) {
+      //   addErrToErrsArr(`${field} must be a valid email`, field);
+      //   return;
+      // }
+      // if (!valueToCheck.split('@')[1].includes('.')) {
+      //   addErrToErrsArr(`${field} must be a valid email`, field);
+      //   return;
+      // }
+    }
+    if (rule.split('-')[0] === 'ref') {
+      const fieldRef = rule.split('-')[1];
+      const valueRef = rule.split('-')[2];
+      if (checkRef(valueToCheck, field, valueRef, fieldRef)) {
+        return;
+      }
+      // if (valueToCheck !== valueRef) {
+      //   addErrToErrsArr("passwords does't match", fieldRef);
+      //   addErrToErrsArr("passwords does't match", field);
+      //   return;
+      // }
+    }
+  }
+}
--- public/js/register.js
+++ public/js/register.js
@@ -1,36 +1,49 @@
 import { BASE_URL } from './modules/fetch.js';
+import { checkInput, clearErrors, errorsFeArr, handleErrors } from './modules/validation.js';
 
 const { formRegister } = document.forms;
 
-async function registerUser(newRegisterObj) {
-  console.log('newRegisterObj ===', newRegisterObj);
+async function registerUser(regInputData) {
+  console.log('regInputData: ', regInputData);
   try {
     const resp = await fetch(`${BASE_URL}/register`, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
       },
-      body: JSON.stringify(newRegisterObj),
+      body: JSON.stringify(regInputData),
     });
     const data = await resp.json();
-    console.log('data:', data);
+    console.log('data: ', data);
     if (data.success) {
       formRegister.reset();
       console.log('registered successfully');
+      handleErrors(data.message);
     }
+    handleErrors(data.message);
   } catch (err) {
-    console.log('error in register:', err);
+    console.log('error in register: ', err);
   }
 }
 
 formRegister.addEventListener('submit', (event) => {
   event.preventDefault();
-  const newRegObj = {
+  const formRegData = {
     fullname: formRegister.fullname.value.trim(),
     email: formRegister.email.value.trim(),
     password: formRegister.password.value.trim(),
     passwordConfirm: formRegister.passwordConfirm.value.trim(),
   };
-  console.log('newRegObj===', newRegObj);
-  registerUser(newRegObj);
+  console.log('formRegData: ', formRegData);
+  clearErrors();
+  checkInput(formRegData.fullname, 'fullname', ['required', 'minLength-3', 'maxLength-255']);
+  checkInput(formRegData.email, 'email', ['required', 'minLength-3', 'email', 'maxLength-255']);
+  checkInput(formRegData.password, 'password', ['required', 'minLength-6', 'maxLength-255']);
+  checkInput(formRegData.passwordConfirm, 'passwordConfirm', [`ref-password-${formRegData.password}`]);
+  // if there are errors in FE
+  if (errorsFeArr.length) {
+    handleErrors();
+    return;
+  }
+  registerUser(formRegData);
 });
