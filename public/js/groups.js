9dc159d6f1ec636f6cab10ce85762c5129e877bb
pocketful <iveta.jac@gmail.com>
2022-05-30 00:27:19 +0300

    create new group from FE, validation

--- public/js/groups.js
+++ public/js/groups.js
@@ -1,5 +1,14 @@
-import { getFetch } from './modules/fetch.js';
+import { getFetch, postFetch } from './modules/fetch.js';
 import createEl from './modules/helpers.js';
+import {
+  checkInput,
+  clearErrors,
+  errorsFeArr,
+  handleErrors,
+  handleErrorsWhileTyping,
+} from './modules/validation.js';
+
+const formCreateGr = document.getElementById('formCreateGroup');
 
 const outputEl = document.querySelector('.groups__cards');
 
@@ -41,3 +50,57 @@ async function getGroups(userToken) {
   }
 }
 getGroups(token);
+
+/* create group ----------------------------------------------- */
+
+async function postGroup(inputData) {
+  console.log('inputData: ', inputData);
+  try {
+    const data = await postFetch('groups', token, inputData);
+    console.log('data: ', data);
+    if (data.success) {
+      formCreateGr.reset();
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+      console.log('New group successfully created.');
+      handleErrors(data.message);
+    } else {
+      console.log('Failed to create new group.');
+      handleErrors(data.message);
+    }
+  } catch (err) {
+    console.log('error in postGroup: ', err);
+  }
+}
+
+// validation rules for inputs
+function getRules() {
+  const rules = {
+    name: ['required', 'minLength-5', 'maxLength-255'],
+  };
+  return rules;
+}
+
+formCreateGr.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const rules = getRules();
+  const formData = {
+    name: formCreateGr.name.value.trim(),
+  };
+  console.log('formData: ', formData);
+  clearErrors();
+
+  // validate input after form submit (valueToCheck, field, rulesArr)
+  checkInput(formData.name, 'name', rules.name);
+
+  // if there are errors in FE
+  if (errorsFeArr.length) {
+    handleErrors();
+    return;
+  }
+  postGroup(formData);
+});
+
+// validate inputs while typing
+handleErrorsWhileTyping(getRules());
--- public/js/modules/fetch.js
+++ public/js/modules/fetch.js
@@ -14,3 +14,21 @@ export async function getFetch(endpoint, token) {
     throw err;
   }
 }
+
+export async function postFetch(endpoint, token, inputData) {
+  try {
+    const resp = await fetch(`${BASE_URL}/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(inputData),
+    });
+    console.log('resp: ', resp);
+    return resp.json();
+  } catch (err) {
+    console.log('error in postFetch:', err);
+    throw err;
+  }
+}
